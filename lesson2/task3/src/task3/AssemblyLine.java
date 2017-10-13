cddf2816fde9fe603b9bde73e7d7324bd1204e2e
kuhcaktyerdna <32371476+kuhcaktyerdna@users.noreply.github.com>
2017-10-13 17:25:55 +0300

    C:/Git/

--- lesson2/task3/src/task3/AssemblyLine.java
+++ lesson2/task3/src/task3/AssemblyLine.java
@@ -5,23 +5,28 @@ public class AssemblyLine implements IAssemblyLine {
 	IProductPart body;
 	IProductPart monitor;
 	IProductPart motherboard;
-
-	public AssemblyLine(IProduct product) {
-		body= ((Product) product).getBody();
-		motherboard= ((Product) product).getMotherboard();
-		monitor= ((Product) product).getMonitor();
-		System.out.println("Installing product parts according to product scheme... \n");
-		product.installFirstPart(body);
-		product.installSecondPart(motherboard);
-		product.installThirdPart(monitor);
+	ILineStep bodyLineStep;
+	ILineStep motherboardLineStep;
+	ILineStep monitorLineStep;
+	
+	
+	
+	public AssemblyLine(ILineStep bodyLineStep, ILineStep motherboardLineStep, ILineStep monitorLineStep, IProduct product) {
+		this.bodyLineStep=bodyLineStep;
+		this.motherboardLineStep= motherboardLineStep;
+		this.monitorLineStep=monitorLineStep;
+		
 
 	}
 
 	@Override
-	public IProduct assembleProduct(IProduct product) {
+	public IProduct assembleProduct(IProduct laptop) {
+		laptop.installFirstPart(bodyLineStep.buildProductPart());
+		laptop.installSecondPart(motherboardLineStep.buildProductPart());
+		laptop.installThirdPart(monitorLineStep.buildProductPart());
 		System.out.println("All product's parts were installed successfully. Assembling is started.");
 		System.out.println("Product was assembled successfuly");
-		return product;
+		return laptop;
 	}
 
 }
