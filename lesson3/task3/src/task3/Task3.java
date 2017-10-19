4b34722b3a74796044d51f5f8e88fdc94568da42
kuhcaktyerdna <32371476+kuhcaktyerdna@users.noreply.github.com>
2017-10-19 21:03:57 +0300

    fix task3

--- lesson3/task3/src/task3/Task3.java
+++ lesson3/task3/src/task3/Task3.java
@@ -18,13 +18,16 @@ public class Task3 {
         Employee tester = new Tester("Tester");
         tester.setSalary(400);
         tester.setRank("Middle");
-        SoftwareFirm softFirm = new SoftwareFirm();
+        SoftwareFirm softFirm = new SoftwareFirm(5);
         softFirm.setName("Firm name");
         Employee[] employees = {relManager, dev, prodManager, progManager, tester,};
-        Employee[] developmentDepartment = softFirm.createDepartment(employees.length, employees);
-        CountingRoom cRoom = new CountingRoom();
-        cRoom.showDepartmentSalary(developmentDepartment);
-
+        employees = softFirm.addToDepartment(tester);
+        employees = softFirm.addToDepartment(dev);
+        employees = softFirm.addToDepartment(prodManager);
+        employees = softFirm.addToDepartment(progManager);
+        employees = softFirm.addToDepartment(relManager);
+        Printer.print(employees.length);
+        Printer.print("There are "+employees.length+" members in department: "+tester.getName()+", "+dev.getName()+", "+prodManager.getName()+", "+progManager.getName()+", "+relManager.getName()+". ");
     }
 
 }
