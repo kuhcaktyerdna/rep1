ffd28a57eb4b6a50707ab7e8a11dcd18cc305c95
kuhcaktyerdna <32371476+kuhcaktyerdna@users.noreply.github.com>
2017-10-19 22:13:57 +0300

    fixed task4

--- lesson3/task4/src/task4/FlightManager.java
+++ lesson3/task4/src/task4/FlightManager.java
@@ -6,63 +6,82 @@ public class FlightManager {
     
     private int canceledFlights;
     private int lateFlights;
+    private Flight[] flights;
+    private int flightNum;
+    private Passenger[] passengers;
+    private int passengerNum;
+    
+    public FlightManager(int numOfFlights, int numOfPassangers){
+        flights = new Flight[numOfFlights];
+        flightNum=0;
+        passengers = new Passenger[numOfPassangers];
+        passengerNum=0;
+    }
+    
+    public Flight[] addFlight(Flight flight){
+        flights[flightNum]=flight;
+        Printer.println("Flight "+flight.getName()+" was added.");
+        flightNum++;
+        return flights;
+    }
+    
+    public Flight[] removeFlight(Flight flight){
+        flights[flightNum]=null;
+        flightNum--;
+        return flights;
+    }
+    
+    public Passenger[] addPassenger(Passenger passenger, Flight flight){
+        if (flight.getStatus()=="canceled") {
+            passenger = null;
+            
+            //Printer.println("Passengers weren't added to "+flight.getName()+". Flight is canceled.");
+        } else if(passengers.length<100) {
+        passengers[passengerNum]=passenger;
+        passengerNum++;}
+        //else Printer.println("Passengers weren't added to "+flight.getName()+". Aircraft is full.");
+        return passengers;
+    }
+    
+    public Passenger[] removePassenger(Passenger passenger){
+        passengers[passengerNum]=null;
+        passengerNum--;
+        return passengers;
+    }
     
     
     
     public Passenger[] managePassengers(Flight flight, Passenger passenger, Passenger[] passengers, String command){
-        if(command.equals("add")) passengers = addPassenger(flight, passenger, passengers);
-        else if(command.equals("remove")) passengers = removePassenger(passenger, passengers);
+        if(command.equals("add")){ 
+            for (int i=0; i<passengers.length; i++){
+                this.passengers = addPassenger(passenger, flight);
+            }
+        }
+        else if(command.equals("remove")) {
+            for (int i=0; i<passengers.length; i++){
+            this.passengers = removePassenger(passenger);
+            }
+        }
         else Printer.print("Error. Wrong command.");
         return passengers;
     }
     
     public Flight[] manageFlights(Flight[] flights, Flight flight){
-        Flight[] newFlights = addFlight(flights, flight);
-        return newFlights;
+        flights = addFlight(flight);
+        return flights;
     }
      
     public void showInfo(Flight[] flights, Passenger[] passengers){
-        showPassengersInformation(passengers);
         checkFlightStatus(flights);
-        showFlightsInformation(flights);
+        showFlightsInformation();
         Printer.println("Number of late flights: "+lateFlights+" . Number of canceled flights: "+canceledFlights);
     }
-    
-    private Flight[] addFlight(Flight[] flights, Flight flight){
-        Flight[] newFlights = new Flight[flights.length+1];
-        System.arraycopy(flights, 0, newFlights, 0, flights.length);
-        newFlights[newFlights.length-1]= flight;
-        Printer.println("Flight "+flight.getName()+" was added.");
-        return newFlights;
-    }
-    
-     private Passenger[] addPassenger(Flight flight, Passenger passenger, Passenger[] passengers) {
-        Passenger[] newPassengers = new Passenger[passengers.length + 1];
-        if ("canceled".equals(flight.getStatus())) {
-            passengers = null;
-            Printer.println("Passengers weren't added to "+flight.getName()+". Flight is canceled.");
-        } else if(passengers.length<100) {
-            System.arraycopy(passengers, 0, newPassengers, 0, passengers.length);
-            newPassengers[newPassengers.length - 1] = passenger;
-        }
-        else Printer.println("Passengers weren't added to "+flight.getName()+". Aircraft is full.");
-        return newPassengers;
-        }
-    
 
-    private Passenger[] removePassenger(Passenger passenger, Passenger[] passengers) {
-        Passenger[] newPassengers = new Passenger[passengers.length - 1];
-        return newPassengers;
-    }
 
-    private void showFlightsInformation(Flight[] flights){
+    private void showFlightsInformation(){
          Printer.print("There are "+flights.length+" flights are going to be started.");
     }
-    
-    private void showPassengersInformation(Passenger[] passengers){
-         Printer.print("There are "+passengers.length+" passengers ready to enter the aircraft.");
-    }
-    
+   
     private void checkFlightStatus(Flight[] flights){
         for(int i=0; i<flights.length-1; i++){
             if(flights[i].getStatus()=="canceled") canceledFlights++;
