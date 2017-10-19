f16a69da8a0846fdc32dbee14685a6a6bd4ef44b
kuhcaktyerdna <32371476+kuhcaktyerdna@users.noreply.github.com>
2017-10-19 22:13:00 +0300

    fixed task4

--- lesson3/task4/src/task4/Task4.java
+++ lesson3/task4/src/task4/Task4.java
@@ -5,33 +5,37 @@ public class Task4 {
     public static void main(String[] args) {
         Airport airport = new Airport("Airport");
         Flight[] flights = new Flight[0];
-        FlightManager flManager = new FlightManager();
+
         Passenger[] passengersForFlight1 = new Passenger[92];
         Passenger[] passengersForFlight2 = new Passenger[100];
         Passenger[] passengersForFlight3 = new Passenger[100];
         Passenger[] passengersForFlight4 = new Passenger[100];
-        Passenger[] passengersForFlight5 = new Passenger[100];
+        Passenger[] passengersForFlight5 = new Passenger[99];
         Passenger passenger = new Passenger();
         Flight flight1 = new Flight("New York - Tokyo ");
         flight1.setStatus("canceled");
-        flights = flManager.manageFlights(flights, flight1);
-        passengersForFlight1 = flManager.managePassengers(flight1, passenger, passengersForFlight1, "add");
         Flight flight2 = new Flight("New York - Berlin ");
         flight2.setStatus("late");
-        flights = flManager.manageFlights(flights, flight2);
-        passengersForFlight2 = flManager.managePassengers(flight2, passenger, passengersForFlight2, "add");
         Flight flight3 = new Flight("New York - Moscow ");
         flight3.setStatus("canceled");
-        flights = flManager.manageFlights(flights, flight3);
-        passengersForFlight3 = flManager.managePassengers(flight3, passenger, passengersForFlight3, "add");
         Flight flight4 = new Flight("New York - Minsk ");
         flight4.setStatus("late");
-        flights = flManager.manageFlights(flights, flight4);
-        passengersForFlight4 = flManager.managePassengers(flight4, passenger, passengersForFlight4, "add");
         Flight flight5 = new Flight("New York - London ");
         flight5.setStatus("normal");
+
+        FlightManager flManager = new FlightManager(5, 500);
+
+        flights = flManager.manageFlights(flights, flight1);
+        flights = flManager.manageFlights(flights, flight2);
+        flights = flManager.manageFlights(flights, flight3);
+        flights = flManager.manageFlights(flights, flight4);
         flights = flManager.manageFlights(flights, flight5);
+        passengersForFlight1 = flManager.managePassengers(flight1, passenger, passengersForFlight1, "add");
+        passengersForFlight2 = flManager.managePassengers(flight2, passenger, passengersForFlight2, "add");
+        passengersForFlight3 = flManager.managePassengers(flight3, passenger, passengersForFlight3, "add");
+        passengersForFlight4 = flManager.managePassengers(flight4, passenger, passengersForFlight4, "add");
         passengersForFlight5 = flManager.managePassengers(flight5, passenger, passengersForFlight5, "add");
+
         flManager.showInfo(flights, passengersForFlight5);
     }
 
